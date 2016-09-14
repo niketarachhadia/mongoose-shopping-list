3ab1a8c4a3153b5d5e5a9964d3670824507599c1
niketarachhadia <niketa.rachhadia@gmail.com>
2016-09-14 16:29:29 -0500

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                            'mongodb://test:test@ds033046.mlab.com:33046/shopping-list-mlab' :
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8089;
